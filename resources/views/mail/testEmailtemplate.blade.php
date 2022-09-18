1cf369b17edd4d2dd6bd2a06d0287691be9f2e7b
Azamat <73704375+azamat1696@users.noreply.github.com>
2022-09-18 20:33:57 +0300

    updated

---
+++ resources/views/mail/testEmailtemplate.blade.php
@@ -0,0 +1,146 @@
+<!-- © 2018 Shift Technologies. All rights reserved. -->
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed;background-color:#f9f9f9" id="bodyTable">
+    <tbody>
+    <tr>
+        <td style="padding-right:10px;padding-left:10px;" align="center" valign="top" id="bodyCell">
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperWebview" style="max-width:600px">
+                <tbody>
+                <tr>
+                    <td align="center" valign="top">
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperBody" style="max-width:600px">
+                <tbody>
+                <tr>
+                    <td align="center" valign="top">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableCard" style="background-color:#fff;border-color:#e5e5e5;border-style:solid;border-width:0 1px 1px 1px;">
+                            <tbody>
+                            <tr>
+                                <td style="background-color:#37474f;font-size:1px;line-height:3px" class="topBorder" height="3">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="padding-bottom: 20px;" align="center" valign="top" class="imgHero">
+                                    <a href="#" style="text-decoration:none" target="_blank">
+                                        <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/hero-img/blue/heroGradient/user-account.png" style="width:100%;max-width:600px;height:auto;display:block;color: #f9f9f9;" width="600">
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding-bottom: 5px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="mainTitle">
+                                    <h2 class="text" style="color:#000;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:28px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:36px;text-transform:none;text-align:center;padding:0;margin:0">Merhaba  {{ $customer->name.' '.$customer->surname }}</h2>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding-bottom: 30px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="subTitle">
+                                    <h4 class="text" style="color:#999;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:16px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:24px;text-transform:none;text-align:center;padding:0;margin:0">Yeni Etkinlik</h4>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding-left:20px;padding-right:20px" align="center" valign="top" class="containtTable ui-sortable">
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableDescription" style="">
+                                        <tbody>
+                                        <tr>
+                                            <td style="padding-bottom: 20px;" align="center" valign="top" class="description">
+                                                <p class="text" style="color:#666;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:14px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:22px;text-transform:none;text-align:center;padding:0;margin:0">
+                                                <h3>{{$event->EventName}} Etkinliği Başlıyor</h3>
+                                                Detaylı bilgi için web sitemizi ziyaret ediniz.
+                                                </p>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableButton" style="">
+                                        <tbody>
+                                        <tr>
+                                            <td style="padding-top:20px;padding-bottom:20px" align="center" valign="top">
+                                                <table border="0" cellpadding="0" cellspacing="0" align="center">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="background-color:#37474F; padding: 12px 35px; border-radius: 50px;" align="center" class="ctaButton">
+                                                            <a  style="color:#fff;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:13px;font-weight:600;font-style:normal;letter-spacing:1px;line-height:20px;text-decoration:none;display:block" target="_blank" class="text" href="http://localhost:9000/event-pages/event/{{$event->id}}" >
+                                                                Detaylı Bilgi
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="font-size:1px;line-height:1px" height="20">&nbsp;</td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" class="space">
+                            <tbody>
+                            <tr>
+                                <td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperFooter" style="max-width:600px">
+                <tbody>
+                <tr>
+                    <td align="center" valign="top">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" class="footer">
+                            <tbody>
+                            <tr>
+                                <td style="padding-top:10px;padding-bottom:10px;padding-left:10px;padding-right:10px" align="center" valign="top" class="socialLinks">
+                                    <a href="#facebook-link" style="display:inline-block" target="_blank" class="facebook">
+                                        <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/facebook.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+                                    </a>
+                                    <a href="#twitter-link" style="display: inline-block;" target="_blank" class="twitter">
+                                        <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/twitter.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+                                    </a>
+                                    <a href="#pintrest-link" style="display: inline-block;" target="_blank" class="pintrest">
+                                        <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/pintrest.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+                                    </a>
+                                    <a href="#instagram-link" style="display: inline-block;" target="_blank" class="instagram">
+                                        <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/instagram.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+                                    </a>
+                                    <a href="#linkdin-link" style="display: inline-block;" target="_blank" class="linkdin">
+                                        <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/linkdin.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 10px 10px 5px;" align="center" valign="top" class="brandInfo">
+                                    <p class="text" style="color:#bbb;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0">©&nbsp;2022 Events. All rights reserved.</p>
+                                </td>
+                            </tr>
+                            <td style="padding-top:10px;padding-bottom:10px;padding-left:10px;padding-right:10px" align="center" valign="top" class="appLinks">
+                                <a href="#Play-Store-Link" style="display: inline-block;" target="_blank" class="play-store">
+                                    <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/app/play-store.png" style="height:auto;margin:5px;width:100%;max-width:120px" width="120">
+                                </a>
+                                <a href="#App-Store-Link" style="display: inline-block;" target="_blank" class="app-store">
+                                    <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/app/app-store.png" style="height:auto;margin:5px;width:100%;max-width:120px" width="120">
+                                </a>
+                            </td>
+                            </tr>
+                            <tr>
+                                <td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
